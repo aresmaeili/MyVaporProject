730ce184d34d073e0c8002f6c79e18725d10690d
AREM <aremiosdev@gmail.com>
2025-08-08 21:38:36 +0330

    aaa

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,10 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("hello") { req async throws -> String in
+          return "سلام از سرور Vapor 🚀"
+      }
 
     try app.register(collection: TodoController())
 }
